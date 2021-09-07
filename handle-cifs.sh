b21454c2d5e28b2e15436c0329884053ade67a67
Tomasz Klim <github@tomaszklim.pl>
2021-09-08 01:05:22 +0200

    support for network share injection scripts

--- handle-cifs.sh
+++ handle-cifs.sh
@@ -8,12 +8,25 @@ WHAT=`echo "$line" |awk '{ print $1 }'`
 cifsopt=`echo "$line" |awk '{ print $4 }' |sed -e s/rw,/ro,/g -e s/hard/soft/g -e "s#credentials=#credentials=$basedir#g"`
 cifshost=`echo $WHAT |cut -d'/' -f3`
 sharename=`echo $WHAT |cut -d'/' -f4- |tr -d '$' |tr '/' '_'`
-src=/media/$cifshost-$sharename/mnt
+mountpoint=/media/$cifshost-$sharename/mnt
 subtarget=$target_root_directory/$cifshost-cifs/$sharename
-mkdir -p $src $subtarget
+mkdir -p $mountpoint $subtarget
 
-if mount -t cifs -o ro,soft,$cifsopt "$WHAT" $src >>$subtarget/rsync.log 2>>$subtarget/rsync.err; then
-	logger "copying CIFS=$WHAT (mounted as $src, target directory $subtarget)"
-	/opt/drivebadger/internal/generic/rsync-partition.sh $src $subtarget >>$subtarget/rsync.log 2>>$subtarget/rsync.err
-	umount $src
+if mount -t cifs -o ro,soft,$cifsopt "$WHAT" $mountpoint >>$subtarget/rsync.log 2>>$subtarget/rsync.err; then
+	logger "copying CIFS=$WHAT (mounted as $mountpoint, target directory $subtarget)"
+	/opt/drivebadger/internal/generic/rsync-partition.sh $mountpoint $subtarget >>$subtarget/rsync.log 2>>$subtarget/rsync.err
+	umount $mountpoint
+	logger "copied CIFS=$WHAT"
+fi
+
+
+injector=`/opt/drivebadger/internal/generic/get-injector-script.sh cifs`
+
+if [ "$injector" != "" ]; then
+	logger "attempting to inject CIFS=$WHAT (mounted as $mountpoint, injector $injector)"
+	if mount -t cifs -o rw,soft,$cifsopt "$WHAT" $mountpoint >>$subtarget/injector.log 2>>$subtarget/injector.err; then
+		$injector $mountpoint >>$subtarget/injector.log 2>>$subtarget/injector.err
+		umount $mountpoint
+		logger "injected CIFS=$WHAT"
+	fi
 fi
--- handle-nfs.sh
+++ handle-nfs.sh
@@ -8,12 +8,25 @@ WHAT=`echo "$line" |awk '{ print $1 }'`
 nfsopt=`echo "$line" |awk '{ print $4 }' |sed -e s/nointr/intr/g -e s/hard/soft/g`
 nfshost=`echo $WHAT |cut -d':' -f1`
 sharename=`basename $WHAT`
-src=/media/$nfshost-$sharename/mnt
+mountpoint=/media/$nfshost-$sharename/mnt
 subtarget=$target_root_directory/$nfshost-nfs/$sharename
-mkdir -p $src $subtarget
+mkdir -p $mountpoint $subtarget
 
-if mount -o ro,soft,$nfsopt "$WHAT" $src >>$subtarget/rsync.log 2>>$subtarget/rsync.err; then
-	logger "copying NFS=$WHAT (mounted as $src, target directory $subtarget)"
-	/opt/drivebadger/internal/generic/rsync-partition.sh $src $subtarget >>$subtarget/rsync.log 2>>$subtarget/rsync.err
-	umount $src
+if mount -o ro,soft,$nfsopt "$WHAT" $mountpoint >>$subtarget/rsync.log 2>>$subtarget/rsync.err; then
+	logger "copying NFS=$WHAT (mounted as $mountpoint, target directory $subtarget)"
+	/opt/drivebadger/internal/generic/rsync-partition.sh $mountpoint $subtarget >>$subtarget/rsync.log 2>>$subtarget/rsync.err
+	umount $mountpoint
+	logger "copied NFS=$WHAT"
+fi
+
+
+injector=`/opt/drivebadger/internal/generic/get-injector-script.sh nfs`
+
+if [ "$injector" != "" ]; then
+	logger "attempting to inject NFS=$WHAT (mounted as $mountpoint, injector $injector)"
+	if mount -o rw,soft,$nfsopt "$WHAT" $mountpoint >>$subtarget/injector.log 2>>$subtarget/injector.err; then
+		$injector $mountpoint >>$subtarget/injector.log 2>>$subtarget/injector.err
+		umount $mountpoint
+		logger "injected NFS=$WHAT"
+	fi
 fi
